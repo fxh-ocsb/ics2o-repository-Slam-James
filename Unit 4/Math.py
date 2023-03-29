3f776aa6b8df146047c9e3d58d523ce083a6dc44
s472773 <s472773@FXHC140m12.ocsb.local>
2023-03-29 10:30:11 -0400

    Today, I learned how to make an input, do math, and be a professional Computer Programmer.

---
+++ Unit 4/Math.py
@@ -0,0 +1,23 @@
+#Samuel James  
+# Math.py
+# March 29, 2023 
+# To Be An Awesome Computer And Do Mathematical Calculations For Me!!!
+
+
+
+# VARIABLE DEFINITION
+Descriptive =  int(input("Enter Number 1:"))
+Descriptive_ish = int(input("Enter Number 2:"))
+
+# INPUT
+
+# PROCESSING
+Adding_vertisement = Descriptive + Descriptive_ish
+Sub_Liminal = Descriptive - Descriptive_ish
+Multi_Talented = Descriptive * Descriptive_ish
+Division_Of_Nations = Descriptive / Descriptive_ish
+# OUTPUT
+print(Descriptive,"+", Descriptive_ish, "=", Adding_vertisement)
+print(Descriptive, "-", Descriptive_ish, "=",  Sub_Liminal)
+print(Descriptive, "*", Descriptive_ish, "=",  Multi_Talented)
+print(Descriptive, "/", Descriptive_ish, "=", Division_Of_Nations)
