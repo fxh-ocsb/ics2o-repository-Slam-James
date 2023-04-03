cb0e66bd71731b4598197fb114bb8e049f9f78ec
s472773 <s472773@FXHC140m12.ocsb.local>
2023-04-03 10:38:12 -0400

    Today, i learned that I never want to deal with tables again. Not kitchen tables, or living room tables, or coffee tables. Enough with the tables though. The Tabulate command was broken so it just irritated me the entire period.

---
+++ Unit 4/Q4_Numbers.py
@@ -0,0 +1,35 @@
+#Samuel James
+#Q4_Numbers
+#April 3, 2023 
+#Display Numbers in a table like format 
+
+
+
+# VARIABLE DEFINITION
+
+# INPUT
+
+# PROCESSING
+
+# OUTPUT
+
+Num_1 =  int(input("Enter Number 1:"))
+Num_2 = int(input("Enter Number 2:"))
+Num_3 =  int(input("Enter Number 3:"))
+Num_4 = int(input("Enter Number 4:"))
+Num_5 =  int(input("Enter Number 5:"))
+
+Sum = (Num_1 + Num_2 + Num_3 + Num_4 + Num_5)
+Average = Sum / 5
+
+#create data
+
+  
+#define header names
+print("|------------------|")
+print("|","Sum", "|" , Sum, "|")
+print("|------------------|")
+print("|Average", "|", Average, "|")
+print("|------------------|")
+print("|", "Nums", "|", Num_1, Num_2, Num_3, Num_4, Num_5, "|")
+print("|------------------|")
