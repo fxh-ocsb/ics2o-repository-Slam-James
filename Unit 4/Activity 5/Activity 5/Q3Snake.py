a98d81ee20c86e101231a02f0c1d280bb2fbaf68
s472773 <s472773@FXHC140m12.ocsb.local>
2023-05-30 10:27:05 -0400

    LETS GOOOOOOO!!!!!

--- Unit 4/Activity 5/Activity 5/Q3Snake.py
+++ Unit 4/Activity 5/Activity 5/Q3Snake.py
@@ -1,7 +1,23 @@
 import pygame
 import time
 import random
- 
+from pygame import mixer
+
+#Instantiate mixer
+mixer.init()
+
+#Load audio file
+mixer.music.load('song.mp3')
+
+print("music started playing....")
+
+#Set preferred volume
+mixer.music.set_volume(0.2)
+
+#Play the music
+mixer.music.play()
+
+
 pygame.init()
  
 white = (255, 255, 255)
@@ -124,4 +140,33 @@ def gameLoop():
     quit()
  
  
-gameLoop()
+#Infinite loop
+while True:
+    gameLoop()
+    print("------------------------------------------------------------------------------------")
+    print("Press 'p' to pause the music")
+    print("Press 'r' to resume the music")
+    print("Press 'e' to exit the program")
+
+    #take user input
+    userInput = input(" ")
+    
+    if userInput == 'p':
+
+        # Pause the music
+        mixer.music.pause()    
+        print("music is paused....")
+    elif userInput == 'r':
+
+        # Resume the music
+        mixer.music.unpause()
+        print("music is resumed....")
+    elif userInput == 'e':
+
+        # Stop the music playback
+        mixer.music.stop()
+        print("music is stopped....")
+        break
+    
+    
+    
